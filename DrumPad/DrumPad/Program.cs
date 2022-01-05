d1831bc840ff241ba86849c820c4afa20b39f27d
Jingwei Zhang <jingweizh@microsoft.com>
2022-01-05 13:35:52 -0800

    adding single instancing

---
+++ DrumPad/DrumPad/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+using Microsoft.UI.Dispatching;
+using Microsoft.Windows.AppLifecycle;
+using Windows.ApplicationModel.Activation;
+using Windows.Storage;
+
+namespace DrumPad
+{
+    class Program
+    {
+        [STAThread]
+        static async Task<int> Main(string[] args)
+        {
+            WinRT.ComWrappersSupport.InitializeComWrappers();
+            bool isRedirect = await DecideRedirection();
+            if (!isRedirect)
+            {
+                Microsoft.UI.Xaml.Application.Start((p) =>
+                {
+                    var context = new DispatcherQueueSynchronizationContext(
+                        DispatcherQueue.GetForCurrentThread());
+                    SynchronizationContext.SetSynchronizationContext(context);
+                    new App();
+                });
+            }
+
+            return 0;
+        }
+
+        private static async Task<bool> DecideRedirection()
+        {
+            bool isRedirect = false;
+
+            AppActivationArguments args = AppInstance.GetCurrent().GetActivatedEventArgs();
+            ExtendedActivationKind kind = args.Kind;
+
+            try
+            {
+                AppInstance keyInstance = AppInstance.FindOrRegisterForKey("randomKey");
+
+                if (keyInstance.IsCurrent)
+                {
+                    keyInstance.Activated += OnActivated;
+                }
+                else
+                {
+                    isRedirect = true;
+                    await keyInstance.RedirectActivationToAsync(args);
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+
+            return isRedirect;
+        }
+
+        private static void OnActivated(object sender, AppActivationArguments args)
+        {
+            ExtendedActivationKind kind = args.Kind;
+
+        }
+    }
+}
